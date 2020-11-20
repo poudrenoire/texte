518ea49b12df41a38992b1de5cbb9aaeed47fb7e
poudrenoire <74770911+poudrenoire@users.noreply.github.com>
2020-11-20 15:12:39 +0100

    Update chapitre1.py
    une autre voie

--- chapitre1.py
+++ chapitre1.py
@@ -1,31 +1,12 @@
 import random
-
-def sujet():
-  s = open("lexique_sujets.txt")
-  sujet = s.readlines()
-  s.close()
-  last = 50
-  rnd = random.randint(0, last)
-  print(sujet[rnd], end='', flush=True)
-  
-def verbe():
-  v = open("lexique_verbes.txt")
-  verbe = v.readlines()
-  v.close()
-  last = 50
-  rnd = random.randint(0, last)
-  print(verbe[rnd], end='', flush=True)
- 
-def complement():
-  c = open("lexique_complements.txt")
-  complement = c.readlines()
-  c.close()
-  last = 50
-  rnd = random.randint(0, last)
-  print(complement[rnd], end='', flush=True)
-
 def phrase():
-  print(' '), sujet(), verbe(), complement(), print('.', end='', flush=True)
+    s = open("lexique_sujets.txt")
+    v = open("lexique_verbes.txt")
+    c = open("lexique_complements.txt")
+    sujet = s.readlines()
+    verbe = v.readlines()
+    complement = c.readlines()
+    print(random.choice(sujet), random.choice(verbe), random.choice(complement), '. ')
 
 def chapitre():
  for _ in range(1250):
