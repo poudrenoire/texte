2c4cb1827d7aabe158de55aefb0bd077cc5183e4
poudrenoire <74770911+poudrenoire@users.noreply.github.com>
2020-11-20 13:16:25 +0100

    Update chapitre1.py
    poser les bases

--- chapitre1.py
+++ chapitre1.py
@@ -1,1 +1,34 @@
+import random
 
+def sujet()
+  s = open("lexique_sujet.txt")
+  sujet = s.readlines()
+  s.close()
+  last = 50
+  rnd = random.randint(0, last)
+  print(sujet[rnd])
+  
+def verbe()
+  v = open("lexique_verbes.txt")
+  verbe = v.readlines()
+  v.close()
+  last = 4999
+  rnd = random.randint(0, last)
+  print(verbe[rnd])
+ 
+def complement()
+  c = open("lexique_complement.txt")
+  complement = c.readlines()
+  c.close()
+  last = 4999
+  rnd = random.randint(0, last)
+  print(complement[rnd])
+
+def phrase()
+  print(' ' + sujet() + verbe() + complement() + '.')
+
+def chapitre()
+ for _ in range(1250):
+    phrase()
+
+chapitre()
