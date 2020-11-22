8d972c771e2f5c4d14411c9fe2c1da16a46a828a
poudrenoire <74770911+poudrenoire@users.noreply.github.com>
2020-11-22 18:12:22 +0100

    Update chapitre1.py

--- chapitre1.py
+++ chapitre1.py
@@ -59,9 +59,6 @@ def phrase_svcc_je():
     print(sujet_r.capitalize(), verbe_r, complement_r, complement2_r, '. ')
 
 def paragraphe():
- phrase_svc_je = phrase_svc_je()
- phrase_asv_je = phrase_asv_je()
- phrase_svcc_je = phrase_svcc_je()
  phraseList = [phrase_svc_je, phrase_asv_je, phrase_svcc_je]
  phraseRand = random.choice(phraseList)
  print(phraseRand)
