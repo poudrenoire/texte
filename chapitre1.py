e88a6f5cafcb068adade557f9a05e13fa8928c77
poudrenoire <74770911+poudrenoire@users.noreply.github.com>
2020-11-22 17:32:39 +0100

    Update chapitre1.py

--- chapitre1.py
+++ chapitre1.py
@@ -61,6 +61,6 @@ def phrase_svcc_je():
 def chapitre():
  phraseList = [phrase_svc_je(), phrase_asv_je(), phrase_svcc_je()]
  for _ in range(250):
-    print(random.choice(phraseList))
+    print(random.sample(phraseList))
 
 chapitre()
