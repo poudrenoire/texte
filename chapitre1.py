acb316c58686fd9b3b812a41ddd9b5f54fddee9e
poudrenoire <74770911+poudrenoire@users.noreply.github.com>
2020-11-25 22:33:58 +0100

    Update chapitre1.py

--- chapitre1.py
+++ chapitre1.py
@@ -86,8 +86,7 @@ def phrase_svc_conc_f_s():
     complement_r = random.choice(complement)   
     print(sujet_r.capitalize(), verbe_r, complement_r, ".", end =" ")
 
-#phrases = ["phrase_svc_je()", "phrase_asv_je()", "phrase_svc_conc_m_s()", "phrase_svcc_je()", "phrase_svc_conc_f_s()"]
-phrases = [1, 2, 3 ,4 ,5]
+phrases = [phrase_svc_je(), phrase_asv_je(), 2, "mot"]
     
 for _ in range (10):
   hasard = random.choices(phrases)
